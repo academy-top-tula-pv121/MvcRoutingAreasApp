5dbce65a0183e72ced8f03416a4705415b10c5ce
maxefimov <efimov.top.academy@gmail.com>
2023-03-07 20:41:26 +0300

    Добавьте файлы проекта.

---
+++ Areas/Works/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcRoutingAreasApp.Areas.Works.Controllers
+{
+    [Area("Works")]
+    public class HomeController : Controller
+    {
+        [Route("{area}/{controller}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
